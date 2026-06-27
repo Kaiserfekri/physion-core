0e1d8e9fd5074cf8b687cd734e15a41c1af954a2
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-27 19:13:42 +0330

    Update nca_graphite.py

--- physion_core/   chemistry/physion_core/chemistry/nca_graphite.py
+++ physion_core/   chemistry/physion_core/chemistry/nca_graphite.py
@@ -1,10 +1,6 @@
-from .base import BaseChemistry
+from physion_core.chemistry.base import BaseChemistry
 
 class NCAGraphiteChemistry(BaseChemistry):
-    """
-    شیمی NCA/Graphite با پارامترهای خوانده‌شده از JSON.
-    """
-
     def __init__(self, params):
         self.p = params
 
