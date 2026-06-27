d1e3a39419f440b41c339b604b2db5f739522ed7
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-27 15:58:00 +0330

    Update functions.py

--- physion_core/   chemistry/physion_core/chemistry/functions.py
+++ physion_core/   chemistry/physion_core/chemistry/functions.py
@@ -1,25 +1,65 @@
 import numpy as np
 
+# -----------------------------
+# Graphite (Anode)
+# -----------------------------
 def U_anode_graphite(soc, T_K):
-    # OCV واقعی گرافیت باید از داده بارگذاری شود
-    raise NotImplementedError
-
-def U_cathode_nmc(soc, T_K):
-    # OCV واقعی NMC باید از داده بارگذاری شود
     raise NotImplementedError
 
 def dU_anode_dT_graphite(soc):
-    # dU/dT گرافیت از مقاله/داده
-    raise NotImplementedError
-
-def dU_cathode_dT_nmc(soc):
-    # dU/dT NMC از مقاله/داده
     raise NotImplementedError
 
 def D_anode_graphite(soc, T_K):
-    # D(soc,T) گرافیت
+    raise NotImplementedError
+
+
+# -----------------------------
+# NMC (Cathode)
+# -----------------------------
+def U_cathode_nmc(soc, T_K):
+    raise NotImplementedError
+
+def dU_cathode_dT_nmc(soc):
     raise NotImplementedError
 
 def D_cathode_nmc(soc, T_K):
-    # D(soc,T) NMC
+    raise NotImplementedError
+
+
+# -----------------------------
+# LFP (Cathode)
+# -----------------------------
+def U_cathode_lfp(soc, T_K):
+    raise NotImplementedError
+
+def dU_cathode_dT_lfp(soc):
+    raise NotImplementedError
+
+def D_cathode_lfp(soc, T_K):
+    raise NotImplementedError
+
+
+# -----------------------------
+# LCO (Cathode)
+# -----------------------------
+def U_cathode_lco(soc, T_K):
+    raise NotImplementedError
+
+def dU_cathode_dT_lco(soc):
+    raise NotImplementedError
+
+def D_cathode_lco(soc, T_K):
+    raise NotImplementedError
+
+
+# -----------------------------
+# NCA (Cathode)
+# -----------------------------
+def U_cathode_nca(soc, T_K):
+    raise NotImplementedError
+
+def dU_cathode_dT_nca(soc):
+    raise NotImplementedError
+
+def D_cathode_nca(soc, T_K):
     raise NotImplementedError
