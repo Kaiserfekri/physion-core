d35242a99a0df837f9d7aa1a6f0a3aedbeb6b5fd
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-27 16:04:16 +0330

    Update lfp_graphite.py

--- physion_core/   chemistry/physion_core/chemistry/lfp_graphite.py
+++ physion_core/   chemistry/physion_core/chemistry/lfp_graphite.py
@@ -13,4 +13,29 @@ class LFPGraphiteChemistry(BaseChemistry):
     def dUeq_dT(self, soc_a, soc_c):
         return self.p["dU_cathode_dT"](soc_c) - self.p["dU_anode_dT"](soc_a)
 
-    # بقیه مثل NMC
+    def capacity_anode(self):
+        return self.p["capacity_anode"]
+
+    def capacity_cathode(self):
+        return self.p["capacity_cathode"]
+
+    def diffusion_coeff_anode(self, soc, T_K):
+        return self.p["D_anode"](soc, T_K)
+
+    def diffusion_coeff_cathode(self, soc, T_K):
+        return self.p["D_cathode"](soc, T_K)
+
+    def kinetics_params_anode(self):
+        return self.p["kinetics_anode"]
+
+    def kinetics_params_cathode(self):
+        return self.p["kinetics_cathode"]
+
+    def electrolyte_params(self):
+        return self.p["electrolyte"]
+
+    def mechanical_params(self):
+        return self.p["mechanical"]
+
+    def degradation_params(self):
+        return self.p["degradation"]
