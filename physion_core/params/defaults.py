24e4f731f92b8017652019dcbac5c08a0e2c1534
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-29 10:01:00 +0330

    Update defaults.py

--- physion_core/params/defaults.py
+++ physion_core/params/defaults.py
@@ -1,1 +1,27 @@
-#q
+"""
+defaults.py
+===========
+
+Default selections for Physion.
+
+Used whenever the user does not explicitly
+choose a chemistry or simulation level.
+"""
+
+# -----------------------------------------
+# Default Chemistry
+# -----------------------------------------
+
+DEFAULT_CHEMISTRY = "lithium_metal"
+
+# -----------------------------------------
+# Default Simulation Level
+# -----------------------------------------
+
+DEFAULT_LEVEL = "basic"
+
+# -----------------------------------------
+# Default Physics Model
+# -----------------------------------------
+
+DEFAULT_MODEL = "SPM"
