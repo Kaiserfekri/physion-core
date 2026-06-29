412a0c2391620af6a11cf54493046d74431ddc29
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-29 09:37:32 +0330

    Update features.py

--- physion_core/params/features.py
+++ physion_core/params/features.py
@@ -1,1 +1,26 @@
-#q
+BASIC = {
+    "spm": True,
+    "thermal": False,
+    "sei": False,
+    "mechanics": False,
+    "tzim": False,
+}
+
+ADVANCED = {
+    "spm": True,
+    "thermal": True,
+    "sei": True,
+    "mechanics": True,
+    "tzim": False,
+}
+
+INDUSTRIAL = {
+    "spm": True,
+    "thermal": True,
+    "sei": True,
+    "mechanics": True,
+    "tzim": True,
+    "uncertainty": True,
+    "calibration": True,
+    "trust": True,
+}
