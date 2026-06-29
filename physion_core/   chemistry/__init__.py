4cb2291d11fa977f7dc92a37515af55ee27333a4
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-29 23:11:52 +0330

    Update __init__.py

--- physion_core/   chemistry/__init__.py
+++ physion_core/   chemistry/__init__.py
@@ -1,1 +1,31 @@
-#q
+"""
+Physion Chemistry Package.
+
+This package contains all chemistry-related
+models and callable functions used by Physion.
+
+Public API
+----------
+- functions
+- function_registry
+"""
+
+from . import functions
+
+from .function_registry import (
+    get_function,
+    has_function,
+    list_functions,
+)
+
+__all__ = [
+
+    "functions",
+
+    "get_function",
+
+    "has_function",
+
+    "list_functions",
+
+]
