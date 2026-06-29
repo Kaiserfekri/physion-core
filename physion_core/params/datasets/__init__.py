c8750276c2b1312f0592065a78cad64ca0157467
Kaiserfekri <114191825+Kaiserfekri@users.noreply.github.com>
2026-06-29 23:10:26 +0330

    Update __init__.py

--- physion_core/params/datasets/__init__.py
+++ physion_core/params/datasets/__init__.py
@@ -1,1 +1,28 @@
-#q
+"""
+Physion parameter datasets.
+
+This package contains all JSON datasets used by
+the Physion parameter loader.
+
+Each JSON file represents one chemistry and one
+simulation level.
+
+Naming convention
+-----------------
+<chemistry>_<anode>_<level>.json
+
+Examples
+--------
+lfp_graphite_simple.json
+lfp_graphite_user.json
+lfp_graphite_industrial.json
+nmc_graphite_simple.json
+"""
+
+from pathlib import Path
+
+DATASET_DIRECTORY = Path(__file__).parent
+
+__all__ = [
+    "DATASET_DIRECTORY",
+]
